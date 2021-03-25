9101fe0e8318a7aaed8c68623a2e5cc29ddc8bae
sreen020 <sjoerdreen@gmail.com>
2021-03-25 17:07:42 +0100

    possible to change order

--- public/js/storeShirt.js
+++ public/js/storeShirt.js
@@ -1,15 +1,26 @@
 export default function storeShirt() {
   const form = document.getElementById("order-form");
   form.addEventListener("submit", function (event) {
-    const { shirtColor, gender, shirtSize, userText, amount } = this.elements;
+    const {
+      shirtColor,
+      gender,
+      shirtSize,
+      userText,
+      amount,
+      id,
+    } = this.elements;
     const data = {
-      id: Date.now(),
+      id: id.value,
       shirtColor: shirtColor.value,
       gender: gender.value,
       shirtSize: shirtSize.value,
       userText: userText.value,
       amount: amount.value,
     };
-    localStorage.setItem("data", JSON.stringify(data));
+    localStorage.setItem(id, JSON.stringify(data));
   });
+}
+
+function createButton() {
+  document.querySelectorAll(".edit-button");
 }
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import bodyParser from "body-parser";
-import express from "express";
+import express, { query } from "express";
 
 import fs from "fs";
+import { request } from "http";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,7 +18,16 @@ app.set("view engine", "ejs");
 
 // Routes
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  if (req.query.edit) {
+    const readableData = JSON.parse(fs.readFileSync("./data.json"));
+    const excistingShirt = readableData.data.find(
+      ({ id }) => id == req.query.edit
+    );
+
+    res.render("index.ejs", { excistingShirt, id: req.query.edit });
+  } else {
+    res.render("index.ejs", { excistingShirt: undefined, id: Date.now() });
+  }
 });
 
 app.get("/shoppingBag", (req, res) => {
@@ -29,15 +39,22 @@ app.get("/shoppingBag", (req, res) => {
 
 app.post("/addShoppingBag", urlencodedParser, (req, res) => {
   const readableData = JSON.parse(fs.readFileSync("./data.json"));
-
-  readableData.data.push(req.body);
+  const excistingShirt = readableData.data.find(({ id }) => id == req.body.id);
+  if (excistingShirt) {
+    excistingShirt.shirtColor = req.body.shirtColor;
+    excistingShirt.gender = req.body.gender;
+    excistingShirt.shirtSize = req.body.shirtSize;
+    excistingShirt.userText = req.body.userText;
+    excistingShirt.amount = req.body.amount;
+  } else {
+    readableData.data.push(req.body);
+  }
 
   const stringData = JSON.stringify(readableData, null, 2);
   fs.writeFileSync("data.json", stringData);
   const amount = readableData.data.length;
-  console.log(amount);
 
-  res.render("shopping-bag.ejs", { data: readableData, amount: amount });
+  res.redirect("/shoppingBag");
 });
 
 // Start server
