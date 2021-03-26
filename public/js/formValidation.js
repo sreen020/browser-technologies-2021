9b028c9f32d5b2840dfe13dadf1640e7990a2692
sreen020 <sjoerdreen@gmail.com>
2021-03-26 12:03:47 +0100

    client-side form validation

---
+++ public/js/formValidation.js
@@ -0,0 +1,25 @@
+export default function formValidation() {
+  const form = document.getElementById("order-form");
+  const colorForm = document.querySelectorAll("#color-form input");
+  const genderForm = document.querySelectorAll("#gender-form input");
+  const sizeForm = document.querySelectorAll("#size-form input");
+  const errorElement = document.getElementById("errorMsg");
+
+  form.addEventListener("submit", (e) => {
+    let messages = [];
+    if (![...colorForm].some((el) => el.checked == true)) {
+      messages.push("Select a color");
+    }
+    if (![...genderForm].some((el) => el.checked == true)) {
+      messages.push("Select a gender");
+    }
+    if (![...sizeForm].some((el) => el.checked == true)) {
+      messages.push("Select a size");
+    }
+
+    if (messages.length > 0) {
+      e.preventDefault();
+      errorElement.innerText = messages.join(", ");
+    }
+  });
+}
--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,9 @@
 import colorShirt from "./colorShirt.js";
 import printText from "./printText.js";
 import storeShirt from "./storeShirt.js";
+import formValidation from "./formValidation.js";
+
 colorShirt();
 storeShirt();
 printText();
+formValidation();
